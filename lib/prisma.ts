1788e0a2df4da29df905da4bcaac418e71a03f3b
haikal <ahmadhaikalmutalib@gmail.com>
2024-11-01 05:53:34 +0800

    fix bugs#3

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  // In development, create a new instance if it doesn't exist
-  prisma = new PrismaClient()
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
